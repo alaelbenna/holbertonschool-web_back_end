bf1387cf13bbc061028ab4b39ff29dc498271311
alaelbenna <elbenna.ala@outlook.com>
2023-01-29 19:38:26 +0100

    add func

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""
-add that takes a float a and a float
-b as arguments and returns their sum as a float.
-"""
+'''
+the Basic annotations - add
+'''
 
 
 def add(a: float, b: float) -> float:
-    """
-    function sum
-    """
+    '''
+    type-annotated func add that takes
+    a float a and a float b as arguments and
+    returns their sums as a float.
+    '''
     return a + b
-    
