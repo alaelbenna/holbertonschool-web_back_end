8890f8a7b59033838a8b63dfbbcea163f75115b3
alaelbenna <elbenna.ala@outlook.com>
2023-01-29 19:41:01 +0100

    func concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -6,7 +6,9 @@ str1 and a string str2 as arguments and returns a
 
 
 def concat(str1: str, str2: str) -> str:
-    """
-    return concat
-    """
+    '''
+    type-annotated func concat that takes a
+    string str1 and a string str2 as arguments and
+    returns concatenated string
+    '''
     return str1 + str2
