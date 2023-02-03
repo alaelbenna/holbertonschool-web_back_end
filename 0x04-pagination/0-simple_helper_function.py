c14e28db0c424993693314094d11f0a23dafbccf
alaelbenna <elbenna.ala@outlook.com>
2023-02-03 17:56:28 +0100

    fix

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+"""
+index
+"""
+
+
 def index_range(page, page_size):
-    # Calculate the start index by multiplying (page - 1) by page_size
+    """
+    index
+    """
     start_index = (page - 1) * page_size
-
-    # Calculate the end index by adding the start index to the page_size
     end_index = start_index + page_size
-
-    # Return the start and end indices as a tuple
     return (start_index, end_index)
