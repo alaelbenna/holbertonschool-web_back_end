b9ca4d89ea60e04b5f8394e4f197d148c1b49033
alaelbenna <elbenna.ala@outlook.com>
2023-03-19 11:10:15 -0700

    Insert a document

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -2,8 +2,8 @@
 """insert new document module"""
 
 def insert_school(mongo_collection, **kwargs):
-    """insert new document in a collection"""
+"""insert new document in a collection"""
 
     if mongo_collection and kwargs:
-                _id = mongo_collection.insert_one(kwargs).inserted_id
-                    return _id
+        _id = mongo_collection.insert_one(kwargs).inserted_id
+    return _id
