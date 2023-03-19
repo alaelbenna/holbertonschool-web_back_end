2505a32f383c84439856036790700d7538f26207
alaelbenna <elbenna.ala@outlook.com>
2023-03-19 11:00:41 -0700

    List all documents

---
+++ 0x0D-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""list all documents module"""
+
+def list_all(mongo_collection):
+    """list all documents in a collection"""
+    document = mongo_collection.find()
+
+    if not document:
+        return []
+    return document
