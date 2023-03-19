9f3a97465a73656d53cc859847ffd01eff0cb7f4
alaelbenna <elbenna.ala@outlook.com>
2023-03-19 11:20:01 -0700

    function that inserts a new document

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
-"""insert new document module"""
+#!/usr/bin/env python3                                                              
+""" pymongo module """
 
 
 def insert_school(mongo_collection, **kwargs):
-     """insert new document in a collection"""
-        result = mongo_collection.insert_one(kwargs)
-            return result.inserted_id
+        """ Insert a document in python """
+            documents = mongo_collection.insert(kwargs)
+                return documents
